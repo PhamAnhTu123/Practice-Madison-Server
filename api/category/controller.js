dda5c06b2eb1189f73f7a03827f4c0ce1bebd179
PhamAnhTu123 <92416931+PhamAnhTu123@users.noreply.github.com>
2022-05-10 09:32:43 +0700

    Add delete category service

--- api/category/controller.js
+++ api/category/controller.js
@@ -1,4 +1,5 @@
 const sequelize = require("../../connection");
+const moment = require('moment');
 const Category = require("../../models/Categories")
 const Product = require("../../models/Products")
 const { tokenExtract } = require("../../services/TokenExtract");
@@ -47,6 +48,25 @@ module.exports.updateOne = async(req, res) => {
   }
 
   category.update(req.body);
+
+  res.status(200).json({ body: category });
+}
+
+module.exports.deletedOne = async (req, res) => {
+  const  { id } = req.params;
+
+  const tokenDecoded = tokenExtract(req);
+
+  if (tokenDecoded.scope !== 'admin') {
+    return res.status(401).send({ message: 'You do not have the access permission' });
+  }
+
+  const category = await Category.findByPk(id);
+  if (!category) {
+    return res.status(400).send({ message: 'Category does not exist' });
+  }
+
+  category.update({ deletedAt: moment() });
 
   res.status(200).json({ body: category });
 }
--- api/category/routes.js
+++ api/category/routes.js
@@ -11,5 +11,6 @@ routes.get('/api/v1/categories', controller.getAll);
 routes.get('/api/v1/categories/:id', validator.params(idParams), controller.getOne);
 routes.post('/api/v1/categories', validator.body(createCategory), controller.createOne);
 routes.put('/api/v1/categories/:id', validator.body(updateCategory), controller.updateOne);
+routes.delete('/api/v1/categories/:id', validator.params(idParams), controller.deletedOne);
 
 module.exports = routes;
